c70b227a9cf06d8ef4bf348dffea0772b2c41a35
santy35 <Allen07150794y>
2018-11-11 12:41:06 -0700

    dialogue testing

---
+++ escape-room/Assets/Scripts/Dialogue.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+
+public class Dialogue{
+    [TextArea(3,10)]//first number is min number of lines the text area will use and second is max
+    public string[] sentences;
+    public string name;
+
+}
---
+++ escape-room/Assets/Scripts/DialogueManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueManager : MonoBehaviour {
+
+
+
+    //Variable that will keep track of the current sentences in our dialouge
+    private Queue<string> sentences;
+
+	// Use this for initialization
+	void Start () {
+        sentences = new Queue<string>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void StartDialogue(Dialogue dialogue){
+        Debug.Log("Starting conversation with " + dialogue.name);
+    }
+}
