9a90b2ead06757c0ab0edc47d90199583abfd512
Tamara Bojovic <32472572+bojotamara@users.noreply.github.com>
2018-11-10 21:31:00 -0700

    Player can 'interact' with object by pressing space

---
+++ escape-room/Assets/Scripts/Interact.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Interact : MonoBehaviour {
+
+    private bool playerinside = false;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag.Equals("Player"))
+        {
+            Debug.Log("Entering...");
+            playerinside = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.tag.Equals("Player"))
+        {
+            Debug.Log("Exiting...");
+            playerinside = false;
+        }
+    }
+    private bool isInteractedWith()
+    {
+        return playerinside && Input.GetKeyUp(KeyCode.Space);
+    }
+
+    private void FixedUpdate()
+    {
+        if (isInteractedWith())
+        {
+            //Run the method to interact with the object
+            Debug.Log("INTERACTION!!!!!");
+        }   
+    }
+
+}
